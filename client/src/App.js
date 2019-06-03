4b0abe80a46c4be33ffd660196b7dd244891e53f
Jonathan Stone <jpstone@protonmail.com>
2019-06-02 22:13:54 -0500

    Added guidance comments to code.

--- client/src/App.js
+++ client/src/App.js
@@ -10,35 +10,50 @@ import './App.css';
 class App extends React.Component {
   constructor() {
     super();
-    this.state = this.initialState = {
+    // Set initial state
+    this.state = {
+      // Mars Rover hazard avoidance cameras
       frontOne: "",
       frontTwo: "",
       rearOne: "",
       rearTwo: "",
+      // Calendar attributes, initial date and focus set to false
       date: moment(),
       focused: false,
+      // Error message for bad, missing or empty data values
       errorMessage: "",
     };
   }
 
   componentDidMount() {
+    // Calls date parser to retreive up to date images for inital render
     this.dateParser();
   }
 
   handleOnSubmit = (event) => {
+    // To prevent browser loading a new page
     event.preventDefault();
+    // In the event that an error message is displayed for lack of data,
+    // the error message is reset so it doesn't trigger in the display function
     this.setState({ errorMessage: "" });
+    // dateParser runs and fetches data, updating state and rerendering the
+    // component and ultimately the display function
     this.dateParser();
   }
 
   dateParser() {
+    // Variable containing the dynamically interpolated string to be added to
+    // the API call, including date and auth key
     const API = `earth_date=${
         this.state.date.format('Y')}-${
           this.state.date.format('M')}-${
             this.state.date.format('D')
               }&api_key=Eet08CsmxY27bMeZJKNogFLg49IjNtsMOdIbWiAN`
+    // API call with dynamically created string added on at the end
     fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?${API}`)
       .then(response => response.json())
+    // Ternary check for bad or missing data followed by setState call
+    // depending on the result 
       .then(data => data.photos && data.photos.length ? 
         this.setState({
           frontOne: data.photos[0].img_src,
@@ -54,10 +69,14 @@ class App extends React.Component {
   }
 
   display() {
-    return this.state.errorMessage ? 
+    // Begins with a ternary check for the presence of an error message
+    return this.state.errorMessage ?
+    // It will display if true
     (
       <div className="errorMessage">{this.state.errorMessage}</div>
     ) : (
+    // If false it will render a child component with the hazard avoidance 
+    // camera images passed in as props
       <ImageOfTheDay
         data-test="imageOfTheDay"
         CameraOne={this.state.frontOne}
@@ -79,6 +98,8 @@ class App extends React.Component {
           <form 
             onSubmit={this.handleOnSubmit}
             data-test="form">
+            {/* SingleDatePicker takes in values from state as well as
+            other attributes from the react-dates API */}
             <SingleDatePicker
               required
               date={this.state.date}
@@ -90,6 +111,8 @@ class App extends React.Component {
               initialVisibleMonth={() => moment().add('months', -5)}
               showDefaultInputIcon
             />
+            {/* Submit updates date and calls dateParser to fetch new data
+            from the API */}
             <Button id="button" type="submit">Submit</Button>
           </form>
         </div>
