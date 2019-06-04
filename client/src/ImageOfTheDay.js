83320f585f6f1a851db8c2361beff5e1728ff6fb
Jonathan Stone <jpstone@protonmail.com>
2019-06-03 22:34:18 -0500

    Removed unused code.

--- client/src/ImageOfTheDay.js
+++ client/src/ImageOfTheDay.js
@@ -3,20 +3,15 @@ import { Image, Modal } from 'semantic-ui-react'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 
-const styles = {
-  height: '90vh',
-  width: '90vh'
-}
-
 const ImageOfTheDay = (props) => (
   <div className="imageOftheDay">
     {props.Cameras.map((img) => (
       <Modal trigger={
         <LazyLoadImage
           key={img}
-          className = "camImg"
+          className="camImg"
           src={img}
-          alt = "NASA1"
+          alt="NASA1"
           effect="blur"
         />
       }>
