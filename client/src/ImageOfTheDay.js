e80029a3af5fad3a32ccd03349bcde951ae964b8
Jonathan Stone <jpstone@protonmail.com>
2019-06-05 11:02:54 -0500

    Added shallow render test for Modal.

--- client/src/ImageOfTheDay.js
+++ client/src/ImageOfTheDay.js
@@ -5,7 +5,7 @@ import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const ImageOfTheDay = (props) => (
   <div className="imageOftheDay">
-    {/* Iterate over props and render images using 3rd party lazy loader component and 
+    {/* Iterate over props, render images using 3rd party lazy loader component and 
         SemanticUI Modal component used for onClick functionality to render larger 
         selected image */}
     {props.Cameras.map((img) => (
--- client/src/ImageOfTheDay.test.js
+++ client/src/ImageOfTheDay.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import 'jest-enzyme';
-import { Image, Modal } from 'semantic-ui-react'
+import { Modal } from 'semantic-ui-react'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import ImageOfTheDay from './ImageOfTheDay';
 
 describe('<ImageOfTheDay />', () => {
 
